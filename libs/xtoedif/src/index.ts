e1ff45b23dd9ccafb4d8912d5580663dcc9ac774
Marius Bosler <70728036+maribox@users.noreply.github.com>
2025-06-17 05:50:41 +0200

    switched to converting to object approach

--- libs/xtoedif/src/index.ts
+++ libs/xtoedif/src/index.ts
@@ -1,33 +1,30 @@
-import { parse, ASTNode, Root, Expression, INode } from "@thi.ng/sexpr";
+import { parse, ASTNode, Root, Expression, INode, Sym } from "@thi.ng/sexpr";
 import ASTQ from 'astq';
 
+function transformASTToObject(node: ASTNode): any {
+  switch (node.type) {
+  case "root":
+  return node.children.map((child) => transformASTToObject(child))
+  case "expr":
+    if (!node.children || node.children.length === 0 || node.children[0].type !== "sym") {
+      throw new Error("invalid file")
+    }
+  return { [(node.children[0] as Sym).value] : node.children.slice(1).map(child => transformASTToObject(child))}
+  case "num":
+  case "str":
+  case "sym":
+  return node.value
+  default:
+  throw new Error("Imported library has an error.")
+  }
+}
 
 export function convertKicadToEdif(netlistContent: string): string {
-  const astq = new ASTQ();
-    astq.adapter({
-        taste: (node: any): boolean => node && typeof node === 'object' && 'type' in node,
-        getParentNode: (node: INode): INode | undefined => {
-            if (node.type === 'root') {
-                return undefined;
-            }
-            return node.parent
-        },
-        getChildNodes: (node: INode): INode[] => {
-            if ('children' in node && Array.isArray(node.children)) {
-                return node.children;
-            }
-            return [];
-        },
-        getNodeType: (node: INode): string => node.type,
-        getNodeAttrNames: (node: INode): string[] => Object.keys(node),
-        getNodeAttrValue: (node: INode, attr: string): INode => (node as INode)[attr]
-    });
-
-
-  const nets = astq.query(parse(netlistContent), '/expr/ expr [ / sym [ @value == "nets" ]]')[0];
-  console.log(nets);
-  console.log(astq.query(nets, '/ expr / expr / str')) 
+  const ast = parse(netlistContent);
+  console.log(ast);
   
+  const structured = transformASTToObject(ast);
+  console.log(structured);
 
   return ""
 }
